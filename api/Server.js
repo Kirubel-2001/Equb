1ff88d76a474090556634f047143cda8a3a01b44
Kirubel Tesfaw <167413403+Kirubel-2001@users.noreply.github.com>
2025-03-20 11:43:51 +0300

    create user model

--- api/Server.js
+++ api/Server.js
@@ -10,10 +10,9 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
-const MONGO_URI = process.env.MONGO_URI;
 
-mongoose.connect(MONGO_URI)
-.then(() => console.log('MongoDB Connected'))
+mongoose.connect(process.env.MONGO)
+.then(() => console.log('MongoDB Connected!'))
 .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
---
+++ api/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["Admin", "Equb Leader", "Participant"],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+const User = mongoose.model("User", userSchema);
+export default User;
