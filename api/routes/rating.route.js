477c8025f1771830d2fc10745540118406b49905
Kirubel Tesfaw <167413403+Kirubel-2001@users.noreply.github.com>
2025-05-08 11:53:00 +0300

    fixed a bug

--- api/routes/rating.route.js
+++ api/routes/rating.route.js
@@ -10,7 +10,7 @@ import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 // Create or update a rating
-router.post("/",  createOrUpdateRating);
+router.post("/",  verifyToken, createOrUpdateRating);
 
 // Get all ratings for a specific Equb
 router.get("/equb/:equbId", getEqubRatings);
